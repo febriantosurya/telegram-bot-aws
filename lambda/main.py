6a8148ea74e471a15a69c94cf5777bf623722e1f
febriantosurya <febriantosurya5@gmail.com>
2024-10-18 09:46:32 +0700

    add input to workflow

--- lambda/main.py
+++ lambda/main.py
@@ -1,7 +1,9 @@
 import json
 import os
 import urllib3
+import requests
 from instance_type import EC2_INSTANCE_TYPES
+
 
 TOKEN = os.environ.get('TOKEN_TELEGRAM')
 if TOKEN is None:
@@ -11,20 +13,33 @@ http = urllib3.PoolManager()
 
 def lambda_handler(event, context):
     body = json.loads(event.get('body', '{}'))
-
     if 'message' in body:
         chat_id = body['message']['chat']['id']
         command = body['message'].get('text', '')
-
         if command == '/ec2':
             send_ec2_keyboard(chat_id)
+        elif command == '/ec2list':
+            send_message(chat_id, "Please wait ...")
+            url = "https://api.github.com/repos/febriantosurya/telegram-bot-aws/actions/workflows/list_server.yml/dispatches"
+            headers = {
+                "Accept": "application/vnd.github+json",
+                "Authorization": "Bearer <token>",
+                "X-GitHub-Api-Version": "2022-11-28"
+            }
+            data = {
+                "ref": "master",
+                "inputs":{
+                    "chat_id": chat_id
+                }
+            }
+            
+            requests.post(url, headers=headers, json=data)
 
     elif 'callback_query' in body:
         # Handle the callback query
         callback_query = body['callback_query']
         chat_id = callback_query['message']['chat']['id']
-        instance_type = callback_query['data']  # The instance type selected
-        
+        instance_type = callback_query['data']
         send_instance_details(chat_id, instance_type)
 
     return {
@@ -34,7 +49,6 @@ def lambda_handler(event, context):
 
 
 def send_ec2_keyboard(chat_id):
-    # Define the inline keyboard for EC2 instance types
     keyboard = {
         "inline_keyboard": [
             [
@@ -58,6 +72,14 @@ def send_ec2_keyboard(chat_id):
 
 
 def send_instance_details(chat_id, instance_type):
+    keyboard = {
+        "inline_keyboard": [
+            [
+                {"text": "Create On-Demand", "callback_data": "ond_t2.micro"},
+                {"text": "Create Spot", "callback_data": "spot_t2.micro"},
+            ]
+        ]
+    }
     if instance_type in EC2_INSTANCE_TYPES:
         instance_info = EC2_INSTANCE_TYPES[instance_type]
         response_text = f"**{instance_type}**\n"
@@ -67,10 +89,26 @@ def send_instance_details(chat_id, instance_type):
         for pricing_type, price in instance_info['pricing'].items():
             response_text += f"- {pricing_type}: {price}\n"
         
-        # Send the response back to the user
-        send_message(chat_id, response_text)
+        # send_message(chat_id, response_text)
+        url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+        data = {
+            'chat_id': chat_id,
+            'text': response_text,
+            'reply_markup': json.dumps(keyboard),
+            'parse_mode': 'Markdown'
+        }
+        encoded_data = json.dumps(data).encode('utf-8')
+        http.request('POST', url, body=encoded_data, headers={'Content-Type': 'application/json'})
     else:
-        send_message(chat_id, "Invalid option selected.")
+        # send_message(chat_id, "Invalid option selected.")
+        url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+        data = {
+            'chat_id': chat_id,
+            'text': "Invalid option selected.",
+            'parse_mode': 'Markdown'  # To enable formatting
+        }
+        encoded_data = json.dumps(data).encode('utf-8')
+        http.request('POST', url, body=encoded_data, headers={'Content-Type': 'application/json'})
 
 def send_message(chat_id, text):
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
