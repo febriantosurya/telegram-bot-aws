1273c5fab0817d83c9a6a5d4ad0936bc7cbf077e
febriantosurya <febriantosurya5@gmail.com>
2024-10-18 16:28:35 +0700

    test output workflow

--- create_ec2_on_demand/main.tf
+++ create_ec2_on_demand/main.tf
@@ -16,23 +16,24 @@ variable "instance_class" {
 provider "aws" {}
 
 # On-Demand Instance
-resource "aws_instance" "server" {
-  ami = "ami-03fa85deedfcac80b"
-  instance_type = var.instance_class
-  tags = {
-    Name = "Telegram-Bot-${formatdate("YYYYMMDD-HHmmss", timestamp())}"
-  }
-  subnet_id = "subnet-011fcaa2eba4610a3"
-  vpc_security_group_ids=["sg-07b0b8944d3dd1bff"]
-  associate_public_ip_address = true
-}
+#resource "aws_instance" "server" {
+#  ami = "ami-03fa85deedfcac80b"
+#  instance_type = var.instance_class
+#  tags = {
+#    Name = "Telegram-Bot-${formatdate("YYYYMMDD-HHmmss", timestamp())}"
+#  }
+#  subnet_id = "subnet-011fcaa2eba4610a3"
+#  vpc_security_group_ids=["sg-07b0b8944d3dd1bff"]
+#  associate_public_ip_address = true
+#  key_name = febri2023
+#}
 
 output "instance_public_ip" {
-  value       = aws_instance.server.public_ip
+  value       = "0.0.0.0" #aws_instance.server.public_ip
   description = "Public IP of the EC2 instance"
 }
 
 output "instance_name" {
-  value       = aws_instance.server.tags["Name"]
+  value       = "test-name" #aws_instance.server.tags["Name"]
   description = "Name of the EC2 instance"
 }
