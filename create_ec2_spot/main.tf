ee8741d43698457aca50bc2e9791248b2627efac
febriantosurya <febriantosurya5@gmail.com>
2024-10-24 21:54:58 +0700

    disable output spot ec2

--- create_ec2_spot/main.tf
+++ create_ec2_spot/main.tf
@@ -9,8 +9,8 @@ terraform {
 
 variable "instance_class" {
   description = "The type of the EC2 instance"
-  type        = string
-  default     = "t2.micro"
+  type = string
+  default = "t2.micro"
 }
 
 provider "aws" {}
@@ -19,9 +19,6 @@ provider "aws" {}
 resource "aws_spot_instance_request" "server" {
   ami = "ami-03fa85deedfcac80b"
   instance_type = var.instance_class
-  tags = {
-    Name = "Telegram-Bot-${formatdate("YYYYMMDD-HHmmss", timestamp())}"
-  }
 	spot_type = "persistent"
   subnet_id = "subnet-011fcaa2eba4610a3"
   vpc_security_group_ids=["sg-07b0b8944d3dd1bff"]
@@ -29,16 +26,22 @@ resource "aws_spot_instance_request" "server" {
 	associate_public_ip_address = true
 }
 
-data "aws_instance" "spot_instance" {
-  instance_id = aws_spot_instance_request.server.spot_instance_id
+resource "aws_tag" "server_tags" {
+  resource_id = aws_spot_instance_request.server.spot_instance_id
+  key         = "Name"
+  value       = "Telegram-Bot-${formatdate("YYYYMMDD-HHmmss", timestamp())}"
 }
 
-output "instance_public_ip" {
-  value       = data.aws_instance.spot_instance.public_ip
-  description = "Public IP of the EC2 instance"
-}
+#data "aws_instance" "spot_instance" {
+#  instance_id = aws_spot_instance_request.server.spot_instance_id
+#}
+#
+#output "instance_public_ip" {
+#  value       = data.aws_instance.spot_instance.public_ip
+#  description = "Public IP of the EC2 instance"
+#}
 
-output "instance_name" {
-  value       = data.aws_instance.spot_instance.tags["Name"]
-  description = "Name of the EC2 instance"
-}
+#output "instance_name" {
+#  value       = data.aws_instance.spot_instance.tags["Name"]
+#  description = "Name of the EC2 instance"
+#}
