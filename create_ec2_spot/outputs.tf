88607d1c42e5547e61f4a7804f14772a69a3942d
febriantosurya <febriantosurya5@gmail.com>
2024-10-31 20:12:27 +0700

    change output

--- create_ec2_spot/outputs.tf
+++ create_ec2_spot/outputs.tf
@@ -1,6 +1,4 @@
 output "instance_public_ips" {
-  value = format("List of IPs:\n%s", join("\\n", [
-    for name, instance in aws_spot_instance_request.server : format("- %s (%s)", name, tostring(instance.public_ip))
-  ]))
+  value = { for name, instance in aws_spot_instance_request.server : name => instance.public_ip }
   description = "Public IPs of the EC2 spot instances"
 }
