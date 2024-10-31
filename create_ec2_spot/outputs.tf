35e3340c63236564d83c830e3ef09aec5d3c4244
febriantosurya <febriantosurya5@gmail.com>
2024-10-31 18:54:50 +0700

    still trying to fix output error

--- create_ec2_spot/outputs.tf
+++ create_ec2_spot/outputs.tf
@@ -1,5 +1,5 @@
 output "instance_public_ips" {
-  value = format("List of IPs:\n%s", join("\n", [
+  value = format("List of IPs:\n%s", join("\\n", [
     for name, instance in aws_spot_instance_request.server : format("- %s (%s)", name, tostring(instance.public_ip))
   ]))
   description = "Public IPs of the EC2 spot instances"
